ce0a00f9b907fe7c25206213441ef3bf7de76970
chu-young <ttheko101@gmail.com>
2021-02-25 16:29:11 +0200

    completed simple color flipper

--- setup/app.js
+++ setup/app.js
@@ -1,1 +1,20 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//Declare global variables
+
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", 'orange', 'yellow', 'blue', '#f10f1f', 'purple', "rgba(13,152,180)"];
+
+const btn = document.getElementById('btn');
+
+const color = document.querySelector('.color');
+//getting a random number
+function getRandomNumber(){
+    return Math.floor(Math.random() * colors.length);
+}
+
+btn.addEventListener('click', function(){
+// get random number between 0-3
+const randomNumber = getRandomNumber();
+console.log(getRandomNumber)
+
+document.body.style.backgroundColor = colors[randomNumber];
+color.textContent = colors[randomNumber];
+});
